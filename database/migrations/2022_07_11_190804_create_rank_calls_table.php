222ae5ad5b9b35dcb87dba46ca9d6a4aedb3b047
Lucas <lucas.breiner@gmx.de>
2022-07-11 23:50:07 +0200

    implement books and calls table
    * stores information about books
    * ranks are saved in the calls tables

---
+++ database/migrations/2022_07_11_190804_create_rank_calls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rank_calls', function (Blueprint $table) {
+            $table->id('call_id');
+            $table->int('bestseller_rank');
+            $table->int('handbook_rank');
+            $table->int('techbook_rank');
+            $table->int('book_id');
+            $table->timestamps();
+            $table->index(['call_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rank_calls');
+    }
+};
