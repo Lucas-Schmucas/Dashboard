b52b0fc3f0ceb1335ef19d8d79fa89dc0846e532
Lucas <lucas.breiner@gmx.de>
2022-08-19 00:16:45 +0200

    adjust format
    * implement the correct class

--- app/Models/RankCall.php
+++ app/Models/RankCall.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Flowframe\Trend\Trend;
-use Flowframe\Trend\TrendValue;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RankCall extends Model
 {
@@ -16,7 +15,7 @@ class RankCall extends Model
   protected $fillable = ['bestseller_rank', 'handbook_rank', 'techbook_rank', 'book_id'];
 
   public function book(): BelongsTo
-   {
+  {
        return $this->belongsTo(Book::class);
    }
 }
